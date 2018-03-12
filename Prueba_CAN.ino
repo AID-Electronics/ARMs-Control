886dc127acb85c0e9c263429cc37727de72936f1
Victor Cadix <victorcadx@hotmail.com>
2018-03-12 11:52:08 +0100

    Funciones send y receive al principio
    Por ordenar un poco el código

--- Prueba_CAN.ino
+++ Prueba_CAN.ino
@@ -10,26 +10,8 @@ unsigned char buf[8];
 char str[20];
 
 MCP_CAN CAN(53);                                      // Set CS to pin 53
- int pot=0;
-void setup()
-{
-    Serial.begin(9600);
- 
-START_INIT:
- 
-    if(CAN_OK == CAN.begin(CAN_500KBPS))                   // init can bus : baudrate = 500k
-    {
-        Serial.println("CAN BUS Shield init ok!");
-    }
-    else
-    {
-        Serial.println("CAN BUS Shield init fail");
-        Serial.println("Init CAN BUS Shield again");
-        delay(100);
-        goto START_INIT;
-    }
-}
- 
+int pot=0;
+
 void sending() {
     // send data:  id = 0x00, standrad flame, data len = 8, stmp: data buf
     pot=map(analogRead(0),0,1024,0,100);
@@ -44,6 +26,7 @@ void sending() {
     CAN.sendMsgBuf(0x00, 0, 8, stmp);
     delay(100);                       // send data per 100ms
 }
+
 void receive(){
     if(CAN_MSGAVAIL == CAN.checkReceive())            // check if data coming
     {
@@ -63,9 +46,27 @@ void receive(){
         Serial.println();
     }
 }
-void loop(){
-sending();
 
+void setup(){
+  Serial.begin(115200);
+  
+START_INIT:
+ 
+    if(CAN_OK == CAN.begin(CAN_500KBPS))                   // init can bus : baudrate = 500k
+    {
+        Serial.println("CAN BUS Shield init ok!");
+    }
+    else
+    {
+        Serial.println("CAN BUS Shield init fail");
+        Serial.println("Init CAN BUS Shield again");
+        delay(100);
+        goto START_INIT;
+    }
 }
+
+void loop(){
+  sending();
+
 }
 
