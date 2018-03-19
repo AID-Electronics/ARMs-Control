08d5748bd6c7574e9218da416d996816e62fb1d0
Victor Cadix <victorcadx@hotmail.com>
2018-03-19 12:37:44 +0100

    Cadenas en header

--- Schneider_LMD_P84.h
+++ Schneider_LMD_P84.h
@@ -161,14 +161,16 @@ void mover (long pasos,long ID){ //pasos debe ser de tipo long para poder contar
   else {
     polarity[4]=0x7F;
   }
-  
-  paquet.i=abs(pasos);
-  char mueve[]={0x23,0x7A,0x60,0x00,paquet.b[0],paquet.b[1],paquet.b[2],paquet.b[3]};
-  
 
+  Paquete p;
+  p.i=abs(pasos);
+  
+  char CadPos1[]={0x23,0x7A,0x60,0x00,p.b[0],p.b[1],p.b[2],p.b[3]}; //Indica la posición a la que ha de moverse
+  char CadPos2[]={0x2B,0x40,0x60,0x00,0x5F,0x00,0x00,0x00};   // son cadenas complementarias para el movimiento que indican el tipo de este: 
+  char CadPos3[]={0x2B,0x40,0x60,0x00,0x4F,0x00,0x00,0x00};   //El movimiento será relativo y no se espera a que acabe antes de procesar el siguiente.
 
   EnviarMSG(polarity,ID);
-  EnviarMSG(mueve,ID);
+  EnviarMSG(CadPos1,ID);
   EnviarMSG(CadPos2,ID);
   EnviarMSG(CadPos3,ID);
   
