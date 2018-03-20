27eb12112081287c03691d17fc5a6be1fd84cac5
Leonardo <leonardo.rivas.micoud96@gmail.com>
2018-03-20 12:23:25 +0100

    he dividido todo en mas funciones
    Ahora a optimizar

--- ARMs-Control.ino
+++ ARMs-Control.ino
@@ -139,10 +139,8 @@ void setup(){
     setupMotor(ID_MOTOR_2,100000,100000,80,51200);
 }
 
-void loop(){
 
-  sensors_event_t event;
-  bno.getEvent (&event);
+void imprimirDatos(sensors_event_t event){
 
   Serial.print ("X: ");
   Serial.print (event.orientation.x,4);
@@ -151,31 +149,7 @@ void loop(){
   Serial.print ("\tZ: ");
   Serial.println (event.orientation.z,4);
 
-  cabeceoPosterior=event.orientation.y*deg2rad; //No estoy demasiado seguro de que sea el eje correcto
-  alabeoPosterior=event.orientation.z*deg2rad;
-  
-  if(abs(cabeceoPosterior-cabeceoAnterior)>TOL || abs(alabeoPosterior-alabeoAnterior)>TOL ) //Esta sentencia se puede omitir
-  {
- //  t=micros();
-pasosMotor1=calcularPasos2D(cabeceoPosterior-cabeceoAnterior,alabeoPosterior-alabeoAnterior,RESOLUCION,RADIO_POLEA,H,333,0,D_REF);
-pasosMotor2=calcularPasos2D(cabeceoPosterior-cabeceoAnterior,alabeoPosterior-alabeoAnterior,RESOLUCION,RADIO_POLEA,H,0,333,D_REF);
-pasosMotor3=calcularPasos2D(cabeceoPosterior-cabeceoAnterior,alabeoPosterior-alabeoAnterior,RESOLUCION,RADIO_POLEA,H,-333,0,D_REF);
-pasosMotor4=calcularPasos2D(cabeceoPosterior-cabeceoAnterior,alabeoPosterior-alabeoAnterior,RESOLUCION,RADIO_POLEA,H,0,-333,D_REF);
- //Serial.print("Micros: ");
-  //Serial.println(micros()-t);
-//pasosMotor1=calcularPasos1D(cabeceoPosterior-cabeceoAnterior,RESOLUCION,RADIO_POLEA,H);
-//pasosMotor3=calcularPasos1D(alabeoPosterior-alabeoAnterior,RESOLUCION,RADIO_POLEA,H);
 
-  //AQUI iría la accion de movimiento
-  mover(pasosMotor1,ID_MOTOR_1);//una vuelta
-  mover(pasosMotor3,ID_MOTOR_2);
-  
-  cabeceoAnterior=cabeceoPosterior;
-  alabeoAnterior=alabeoPosterior;
-  }
-  
-  
-      
   Serial.print ("Pasos motor 1: ");
   Serial.print (pasosMotor1);
   Serial.print ("        Pasos motor 2: ");
@@ -186,6 +160,49 @@ pasosMotor4=calcularPasos2D(cabeceoPosterior-cabeceoAnterior,alabeoPosterior-ala
   Serial.print (pasosMotor4);
   Serial.println(" ");
   //delay (2);
+  
+}
+
+
+void moverMotores() {
+
+
+  
+  if(abs(cabeceoPosterior-cabeceoAnterior)>TOL || abs(alabeoPosterior-alabeoAnterior)>TOL ) //Esta sentencia se puede omitir
+  {
+      //  t=micros();
+      pasosMotor1=calcularPasos2D(cabeceoPosterior-cabeceoAnterior,alabeoPosterior-alabeoAnterior,RESOLUCION,RADIO_POLEA,H,333,0,D_REF);
+      pasosMotor2=calcularPasos2D(cabeceoPosterior-cabeceoAnterior,alabeoPosterior-alabeoAnterior,RESOLUCION,RADIO_POLEA,H,0,333,D_REF);
+      pasosMotor3=calcularPasos2D(cabeceoPosterior-cabeceoAnterior,alabeoPosterior-alabeoAnterior,RESOLUCION,RADIO_POLEA,H,-333,0,D_REF);
+      pasosMotor4=calcularPasos2D(cabeceoPosterior-cabeceoAnterior,alabeoPosterior-alabeoAnterior,RESOLUCION,RADIO_POLEA,H,0,-333,D_REF);
+      //Serial.print("Micros: ");
+      //Serial.println(micros()-t);
+      //pasosMotor1=calcularPasos1D(cabeceoPosterior-cabeceoAnterior,RESOLUCION,RADIO_POLEA,H);
+      //pasosMotor3=calcularPasos1D(alabeoPosterior-alabeoAnterior,RESOLUCION,RADIO_POLEA,H);
+
+      //AQUI iría la accion de movimiento
+      mover(pasosMotor1,ID_MOTOR_1);//una vuelta
+      mover(pasosMotor3,ID_MOTOR_2);
+  
+      cabeceoAnterior=cabeceoPosterior;
+      alabeoAnterior=alabeoPosterior;
+  }
+
+      
+  
+}
+
+void loop(){
+
+  sensors_event_t event;
+  bno.getEvent (&event);
+
+  imprimirDatos(event);
+
+  cabeceoPosterior=event.orientation.y*deg2rad; //No estoy demasiado seguro de que sea el eje correcto
+  alabeoPosterior=event.orientation.z*deg2rad;
+  
+  moverMotores();  
 
 }
 
