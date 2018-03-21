2c98e07790f1c188171a76c41b79d6d06f7c9f5f
Luis Martínez <lu.marza00@gmail.com>
2018-03-21 12:38:01 +0100

    implementado
    Se han guardad los datos leidos por la IMU en una matriz y se han ido escogiendo uno a uno en el for e introduciendo esos datos a las funciones de movimiento

--- ARMs-Control.ino
+++ ARMs-Control.ino
@@ -142,7 +142,61 @@ void setup(){
     setupMotor(ID_MOTOR_1,100000,100000,80,51200); //(long ID_motor,uint32_t Acel,uint32_t Decel, int current ,uint32_t MaxVel )
     setupMotor(ID_MOTOR_2,100000,100000,80,51200);
 
-    long matrix[][3]={
+    
+}
+
+
+void imprimirDatos(sensors_event_t event){
+
+  Serial.print ("X: ");
+  Serial.print (event.orientation.x,4);
+  Serial.print ("\tY: ");
+  Serial.print (event.orientation.y,4);
+  Serial.print ("\tZ: ");
+  Serial.println (event.orientation.z,4);
+
+
+  Serial.print ("Pasos motor 1: ");
+  Serial.print (pasosMotor1);
+  Serial.print ("        Pasos motor 2: ");
+  Serial.print (pasosMotor2);
+  Serial.print ("      Pasos motor 3: ");
+  Serial.print (pasosMotor3);
+  Serial.print ("    Pasos motor 4: ");
+  Serial.print (pasosMotor4);
+  Serial.println(" ");
+  //delay (2);
+  
+}
+
+
+void moverMotores() {
+
+
+  
+  if(abs(cabeceoPosterior-cabeceoAnterior)>TOL || abs(alabeoPosterior-alabeoAnterior)>TOL ) //Esta sentencia se puede omitir
+  {
+      
+      pasosMotor1=calcularPasos2D(cabeceoPosterior-cabeceoAnterior,alabeoPosterior-alabeoAnterior,RESOLUCION,RADIO_POLEA,H,333,0,D_REF);
+      pasosMotor2=calcularPasos2D(cabeceoPosterior-cabeceoAnterior,alabeoPosterior-alabeoAnterior,RESOLUCION,RADIO_POLEA,H,0,333,D_REF);
+      pasosMotor3=calcularPasos2D(cabeceoPosterior-cabeceoAnterior,alabeoPosterior-alabeoAnterior,RESOLUCION,RADIO_POLEA,H,-333,0,D_REF);
+      pasosMotor4=calcularPasos2D(cabeceoPosterior-cabeceoAnterior,alabeoPosterior-alabeoAnterior,RESOLUCION,RADIO_POLEA,H,0,-333,D_REF);
+      
+      //pasosMotor1=calcularPasos1D(cabeceoPosterior-cabeceoAnterior,RESOLUCION,RADIO_POLEA,H);
+      //pasosMotor3=calcularPasos1D(alabeoPosterior-alabeoAnterior,RESOLUCION,RADIO_POLEA,H);
+
+      //AQUI iría la accion de movimiento
+      mover(pasosMotor1,ID_MOTOR_1);//una vuelta
+      mover(pasosMotor2,ID_MOTOR_2);
+  
+      cabeceoAnterior=cabeceoPosterior;
+      alabeoAnterior=alabeoPosterior;
+  }
+
+      
+  
+}
+long matrix[][3]={
   {0.215969994664, 0.0114713395014, 1.22666156292},
 {0.213174015284, 0.0056540989317, 1.22521841526},
 {0.210524126887, 0.00119585474022, 1.22429478168},
@@ -685,68 +739,8 @@ void setup(){
 
 
     
-  for(int i=0;i<MAX;i++)
-  {
-  cabeceoPosterior=matrix[i][0]; //No estoy demasiado seguro de que sea el eje correcto
-  alabeoPosterior=matrix[i][1];
-  moverMotores(); 
-
-  delay(10);
-  }
-}
-
-
-void imprimirDatos(sensors_event_t event){
-
-  Serial.print ("X: ");
-  Serial.print (event.orientation.x,4);
-  Serial.print ("\tY: ");
-  Serial.print (event.orientation.y,4);
-  Serial.print ("\tZ: ");
-  Serial.println (event.orientation.z,4);
-
-
-  Serial.print ("Pasos motor 1: ");
-  Serial.print (pasosMotor1);
-  Serial.print ("        Pasos motor 2: ");
-  Serial.print (pasosMotor2);
-  Serial.print ("      Pasos motor 3: ");
-  Serial.print (pasosMotor3);
-  Serial.print ("    Pasos motor 4: ");
-  Serial.print (pasosMotor4);
-  Serial.println(" ");
-  //delay (2);
   
-}
-
-
-void moverMotores() {
-
-
   
-  if(abs(cabeceoPosterior-cabeceoAnterior)>TOL || abs(alabeoPosterior-alabeoAnterior)>TOL ) //Esta sentencia se puede omitir
-  {
-      
-      pasosMotor1=calcularPasos2D(cabeceoPosterior-cabeceoAnterior,alabeoPosterior-alabeoAnterior,RESOLUCION,RADIO_POLEA,H,333,0,D_REF);
-      pasosMotor2=calcularPasos2D(cabeceoPosterior-cabeceoAnterior,alabeoPosterior-alabeoAnterior,RESOLUCION,RADIO_POLEA,H,0,333,D_REF);
-      pasosMotor3=calcularPasos2D(cabeceoPosterior-cabeceoAnterior,alabeoPosterior-alabeoAnterior,RESOLUCION,RADIO_POLEA,H,-333,0,D_REF);
-      pasosMotor4=calcularPasos2D(cabeceoPosterior-cabeceoAnterior,alabeoPosterior-alabeoAnterior,RESOLUCION,RADIO_POLEA,H,0,-333,D_REF);
-      
-      //pasosMotor1=calcularPasos1D(cabeceoPosterior-cabeceoAnterior,RESOLUCION,RADIO_POLEA,H);
-      //pasosMotor3=calcularPasos1D(alabeoPosterior-alabeoAnterior,RESOLUCION,RADIO_POLEA,H);
-
-      //AQUI iría la accion de movimiento
-      mover(pasosMotor1,ID_MOTOR_1);//una vuelta
-      mover(pasosMotor3,ID_MOTOR_2);
-  
-      cabeceoAnterior=cabeceoPosterior;
-      alabeoAnterior=alabeoPosterior;
-  }
-
-      
-  
-}
-
 void loop(){
       Serial.print("Micros: ");
       Serial.println(micros()-t);
@@ -755,9 +749,17 @@ void loop(){
 
   //imprimirDatos(event);
 
-   
+   for(int i=0;i<MAX;i++)
+  {
+  cabeceoPosterior=matrix[i][1]; //No estoy demasiado seguro de que sea el eje correcto
+  alabeoPosterior=matrix[i][0];
+  moverMotores(); 
+
+  delay(1);
   
   t=micros();
+  }
+  exit(0);
 }
 
 
