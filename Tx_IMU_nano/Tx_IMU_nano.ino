c208d148b44f139a0695da3080b5df93da3ed097
Luis Martínez <lu.marza00@gmail.com>
2018-04-19 18:58:46 +0200

    radio

--- Tx_IMU_nano/Tx_IMU_nano.ino
+++ Tx_IMU_nano/Tx_IMU_nano.ino
@@ -4,13 +4,13 @@ void setup()
 {
   // Initialize the IO and ISR
   vw_setup(2000); // Bits per sec -->150m
-  vw_set_tx_pin(3);//Aqeui se selecciona el pin digital que manda la información
+  vw_set_tx_pin(3);//Aqui se selecciona el pin digital que manda la información
 }
 
 void loop()
 {
   send("--INCINACIÓN IMU2--");
-  delay(1000);
+  delay(100);
 }
 
 void send (char *message)
