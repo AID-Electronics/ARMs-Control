5d0ee02819f47a3c37fe44bd0359aeabd3290c62
Luis Martínez <lu.marza00@gmail.com>
2018-04-23 10:16:19 +0200

    ; al final de la linea

--- nano_transmiter/nano_transmiter.ino
+++ nano_transmiter/nano_transmiter.ino
@@ -13,12 +13,14 @@ void loop(){
    IMU_movil.update(); 
    Serial.print(IMU_movil.orientacion.z,4); 
    Serial.print(",");
-   Serial.println(IMU_movil.orientacion.y,4);
-  //SERIAL 1
+   Serial.print(IMU_movil.orientacion.y,4);
+   Serial.println(";");
+//SERIAL 1
    Serial1.print("=");
    Serial1.print(IMU_movil.orientacion.z,4); 
    Serial1.print(",");
-   Serial1.println(IMU_movil.orientacion.y,4);
+   Serial1.print(IMU_movil.orientacion.y,4);
+   Serial1.println(";");
 
     if(Serial.read()!=-1){
        IMU_movil.imprimirDatos();
