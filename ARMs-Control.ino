baf4c4c551b873bfee184f06504271842dce8c30
Victor Cadix <victorcadx@hotmail.com>
2018-04-23 14:06:16 +0200

    Class plataforma

--- ARMs-Control.ino
+++ ARMs-Control.ino
@@ -9,28 +9,34 @@
 #include "IMU.h"
 
 IMU IMU_fija;
-double IMU_movil_Z;
-double IMU_movil_Y;
-double zrad;
-double yrad;
 
-bool calibrarPlat(){
-  while(abs(IMU_movil_Y)>0.5 || abs(IMU_movil_Z)>0.5){
-    //leerRF(); //funcion que no existe aun
-    yrad = (-IMU_movil_Y )* deg2rad;
-    zrad = (-IMU_movil_Z )* deg2rad;
+class Plataforma{
+  Vector3D orientacion;
+  Vector3D accel;
+  double zrad;
+  double yrad;
+
+  bool calibrarPlat();
+};
+
+bool Plataforma::calibrarPlat(){
+  while(abs(orientacion.y)>0.5 || abs(orientacion.z)>0.5){
+
+    Vector3D aux;
+    getOrientRF(aux);
+    yrad = (-orientacion.y )* deg2rad;
+    zrad = (-orientacion.z )* deg2rad;
     
     pasosMotor1 = calcularPasos2D(yrad, zrad, RESOLUCION, RADIO_POLEA, H, 333, 0, D_REF);
     pasosMotor2 = calcularPasos2D(yrad, zrad, RESOLUCION, RADIO_POLEA, H, 0, 333, D_REF);
     pasosMotor3 = calcularPasos2D(yrad, zrad, RESOLUCION, RADIO_POLEA, H, -333, 0, D_REF);
     pasosMotor4 = calcularPasos2D(yrad, zrad, RESOLUCION, RADIO_POLEA, H, 0, -333, D_REF);
   
-  moverRelatEspera(pasosMotor1, ID_MOTOR_1); //movimientos relativos con espera
-  moverRelatEspera(pasosMotor2, ID_MOTOR_2);
-  //moverRelatEspera(pasosMotor3,ID_MOTOR_3);
-  //moverRelatEspera(pasosMotor4,ID_MOTOR_4);
+    moverRelatEspera(pasosMotor1, ID_MOTOR_1); //movimientos relativos con espera
+    moverRelatEspera(pasosMotor2, ID_MOTOR_2);
+    //moverRelatEspera(pasosMotor3,ID_MOTOR_3);
+    //moverRelatEspera(pasosMotor4,ID_MOTOR_4);
   
-   //leerRF(); //funcion que no existe aun
   }
      //apagar motores()//funcion para apagar motores para que la posicien absoluta de cero pasos coincida con al horizonte 
     setupMotor(ID_MOTOR_1,1000000,1000000,100,512000);
@@ -62,8 +68,6 @@ void loop(){
     IMU_fija.printTemp();
     IMU_fija.print();
   }*/
-
-  getOrientExterna();
   
 }
 
