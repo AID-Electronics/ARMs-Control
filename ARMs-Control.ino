56f344e101f10d8be97b5452732132fbb648b4c3
Victor Cadix <victorcadx@hotmail.com>
2018-04-23 10:18:15 +0200

    Funcionando

--- ARMs-Control.ino
+++ ARMs-Control.ino
@@ -53,9 +53,10 @@ long pos;
 bool RxStart = 0;
 bool RxEnd = 0;
 unsigned int RxCont = 0;
-String RxBuff[8];
+char RxBuff[9];
 bool negativo;
 double alabeo;
+double cabeceo;
 
 void loop(){
  /* IMU_fija.update();
@@ -70,30 +71,50 @@ void loop(){
 
   if (Serial1.available()){
     char token = Serial1.read();
-    Serial.print(token);
+    //Serial.print(token);
     if(token == '='){
       RxStart = 1;
       RxCont = 0;
       negativo = 0;
     }
     else{
-      if(token = ';'){
-        RxBuff[RxCont] = NULL;
+      if(token == ','){
+        RxBuff[RxCont] = '\0';
         RxCont = 0;
-        alabeo = RxBuff.toDouble();
+        
+        char *p;
+        alabeo = strtod(RxBuff,&p);
         if (negativo == true){
           alabeo = 0 - alabeo;
+          negativo = false;
         }
+        Serial.print("   Alabeo: ");
+        Serial.print(alabeo,4);
       }
       else if (token == '-'){
         negativo = true;
+      }
+      else if (token == ';'){
+        RxStart = 0;
+        RxBuff[RxCont] = '\0';
+        RxCont = 0;
+        //Serial.print("\tBuffer: ");
+        //Serial.print(RxBuff);
+        
+        char *p;
+        cabeceo = strtod(RxBuff,&p);
+        if (negativo == true){
+          cabeceo = 0 - cabeceo;
+          negativo = false;
+        }
+        Serial.print("\tCabeceo: ");
+        Serial.println(cabeceo,4);
       }
       else if (RxStart == true){
         RxBuff[RxCont] = token;
         RxCont++;
       }
     }
-    
     
   }
   
