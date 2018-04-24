0124a879df97d450350613acbf20ce22c447bf52
Victor Cadix <victorcadx@hotmail.com>
2018-04-24 10:26:19 +0200

    paso de las aceleraciones por RF

--- nano_transmiter/nano_transmiter.ino
+++ nano_transmiter/nano_transmiter.ino
@@ -2,25 +2,38 @@
 
 IMU IMU_movil;
  int i=0;
+
+unsigned long tiempo;
+unsigned long past_tiempo;
+unsigned long tiempo_muestreo;
+
 void setup(){
     Serial1.begin(4800);     
     Serial.begin(115200);
     IMU_movil.setup();
-    
+    past_tiempo = 0;
+    tiempo_muestreo = 100;
 }
  
 void loop(){ 
-   IMU_movil.update(); 
-   Serial.print(IMU_movil.orientacion.z,4); 
+   IMU_movil.update();
+   tiempo = millis();
+   if (tiempo - past_tiempo >= tiempo_muestreo){
+    past_tiempo = tiempo;
+   Serial.print(IMU_movil.accel.x,4);
    Serial.print(",");
-   Serial.print(IMU_movil.orientacion.y,4);
+   Serial.print(IMU_movil.accel.y,4);
+   Serial.print(",");
+   Serial.print(IMU_movil.accel.z,4);
    Serial.println(";");
 //SERIAL 1
-   Serial1.print("=");
-   Serial1.print(IMU_movil.orientacion.z,4); 
+   Serial1.print(IMU_movil.accel.x,4); 
    Serial1.print(",");
-   Serial1.print(IMU_movil.orientacion.y,4);
+   Serial1.print(IMU_movil.accel.y,4);
+   Serial1.print(",");
+   Serial1.print(IMU_movil.accel.z,4);
    Serial1.println(";");
+   }
 
     if(Serial.read()!=-1){
        IMU_movil.imprimirDatos();
