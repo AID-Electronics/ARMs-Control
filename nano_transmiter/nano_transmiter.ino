ba16d656bef50a96042c4ea6f550d7aefba67963
Victor Cadix <victorcadx@hotmail.com>
2018-04-24 11:17:14 +0200

    Sin tiempo de muestreo RF

--- nano_transmiter/nano_transmiter.ino
+++ nano_transmiter/nano_transmiter.ino
@@ -3,23 +3,16 @@
 IMU IMU_movil;
  int i=0;
 
-unsigned long tiempo;
-unsigned long past_tiempo;
-unsigned long tiempo_muestreo;
-
 void setup(){
     Serial1.begin(4800);     
     Serial.begin(115200);
     IMU_movil.setup();
-    past_tiempo = 0;
-    tiempo_muestreo = 100;
+
 }
  
-void loop(){ 
+void loop(){
    IMU_movil.update();
-   tiempo = millis();
-   if (tiempo - past_tiempo >= tiempo_muestreo){
-    past_tiempo = tiempo;
+
    Serial.print(IMU_movil.accel.x,4);
    Serial.print(",");
    Serial.print(IMU_movil.accel.y,4);
@@ -33,7 +26,6 @@ void loop(){
    Serial1.print(",");
    Serial1.print(IMU_movil.accel.z,4);
    Serial1.println(";");
-   }
 
     if(Serial.read()!=-1){
        IMU_movil.imprimirDatos();
