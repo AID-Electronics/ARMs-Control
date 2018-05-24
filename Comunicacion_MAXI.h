87a75495d1ea7db8330b306ae751d4e01d5cf9cc
Victor Cadix <victorcadx@hotmail.com>
2018-05-24 10:15:20 +0200

    Setup

--- Comunicacion_MAXI.h
+++ Comunicacion_MAXI.h
@@ -1,4 +1,17 @@
 #ifndef COMUNICACION_MAXI_H
 #define COMUNICACION_MAXI_H
 
+#define pinEstado A0;
+
+class Comunicacion_MAXI{
+
+  void setup();
+}
+
+void Comunicacion_MAXI::setup(){
+  Serial3.begin(115200);
+  Controllino_RS485Init();
+  pinMode(pinEstado,OUTPUT);
+}
+
 #endif
