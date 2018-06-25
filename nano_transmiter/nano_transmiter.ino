b4c5f0e9f528010142e8b7743b28639fb0c278b7
Victor Cadix <victorcadx@hotmail.com>
2018-06-25 12:22:53 +0200

    pinReset setup

--- nano_transmiter/nano_transmiter.ino
+++ nano_transmiter/nano_transmiter.ino
@@ -1,4 +1,6 @@
- #include "IMU.h"
+#include "IMU.h"
+
+#define pinReset 2
 
 IMU IMU_movil;
  int i=0;
@@ -6,6 +8,8 @@ IMU IMU_movil;
 void setup(){
     //Serial1.begin(4800);  
     Serial.begin(4800);
+    pinMode(pinReset,OUTPUT);
+    digitalWrite(pinReset,HIGH);
     IMU_movil.setup();
 
 }
@@ -30,6 +34,7 @@ void loop(){
 
     if(Serial.read()!=-1){
        IMU_movil.imprimirDatos();
+       IMU_movil.reset(pinReset);
     //IMU_movil.displayCalStatus();
     //IMU_movil.printTemp();
     //IMU_movil.print();
