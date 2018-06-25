2bdf282538968d68666bc4550f5f30fecdf6ac6e
Victor Cadix <victorcadx@hotmail.com>
2018-06-25 12:38:23 +0200

    Check accel before send

--- nano_transmiter/nano_transmiter.ino
+++ nano_transmiter/nano_transmiter.ino
@@ -15,14 +15,20 @@ void setup(){
 }
  
 void loop(){
-   IMU_movil.update();
-  Serial.print("=");
-   Serial.print(IMU_movil.accel.x,4);
-   Serial.print(",");
-   Serial.print(IMU_movil.accel.y,4);
-   Serial.print(",");
-   Serial.print(IMU_movil.accel.z,4);
-   Serial.println(";");
+    IMU_movil.update();
+    if (IMU_movil.isAccelDataCorrect()){
+        Serial.print("=");
+        Serial.print(IMU_movil.accel.x,4);
+        Serial.print(",");
+        Serial.print(IMU_movil.accel.y,4);
+        Serial.print(",");
+        Serial.print(IMU_movil.accel.z,4);
+        Serial.println(";");
+    }
+    else{
+        IMU_movil.reset(pinReset);
+    }
+   
 //SERIAL 1
 //  Serial1.print("=");
 //   Serial1.print(IMU_movil.accel.x,4); 
